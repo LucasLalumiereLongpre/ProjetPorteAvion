42acbc8c04d49d9f5ef8d76431379df56a12916d
LucasLalumiereLongpre <lucaslalumierelongpre@gmail.com>
2025-04-21 15:32:36 -0400

    fonction launchPlane

--- Code/plane.py
+++ Code/plane.py
@@ -7,7 +7,7 @@ Description: Simulation des differentes phases d une avion sur un porte-avion
 import time
 import random
 from enum import Enum
-from progressbar import progressbar
+import progressbar
 
 # Etats possibles d un avion
 class PlaneStates(Enum):
@@ -24,8 +24,36 @@ class Plane:
     def __init__(self, id):
         self.id = id
         self.status = PlaneStates.InHangar
+        self.progress = 0
 
-    def fonction(self):
-        pass
+    def getId(self):
+        return self.id
+    
+    def getStatus(self):
+        return self.status
+    
+    def setStatus(self, status):
+        self.status = status
 
+    def launchPlane(self):
+        duration = 5  # seconds
+        steps = 100
+        interval = duration / steps  # time between increments
 
+        widgets = [' [',
+         progressbar.Timer(format= 'elapsed time: %(elapsed)s'),
+         '] ',
+           progressbar.Bar('#' ),' (', 
+           progressbar.ETA(), ') ',
+          ]
+        bar = progressbar.ProgressBar(max_value=steps, widgets=widgets).start()
+        
+        self.setStatus(PlaneStates.Launching)
+        for i in range(steps + 1):  # 0 to 100 inclusive
+            self.progress = i
+            time.sleep(interval)
+            bar.update(i)
+        self.setStatus(PlaneStates.InAir)
+
+    def getProgress(self):
+        return self.progress
